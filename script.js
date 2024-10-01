1d1caa7b2a4b0a5eb2bc25e07df5c25692ae6e6d
Gabriel Galindo de Almeida <zatera_@hotmail.com>
2024-10-01 12:10:22 -0300

    Add functions

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-
-const input = document.querySelector("#inputItem")
-const btn = document.querySelector("#btn")
-const html = document.createElement("div")
-const main = document.querySelector("main")
+const input = document.querySelector("#inputItem");
+const btn = document.querySelector("#btn");
+const main = document.querySelector("main");
 
 btn.addEventListener('click', (event) => {
-  event.preventDefault()
+  event.preventDefault();
 
-  html.classList.add("wrapper-box");
+  // Criar um novo div para cada item
+  const newDiv = document.createElement("div");
+  newDiv.classList.add("wrapper-box");
 
-  html.innerHTML = `
+  newDiv.innerHTML = `
     <div class="box-name">
       <input type="checkbox" name="item" id="pao">
       <label for="pao">${input.value}</label>
@@ -17,8 +17,9 @@ btn.addEventListener('click', (event) => {
     <img src="assets/frame-3.svg" alt="Excluir item da lista">
   `;
 
-  main.appendChild(html)
+  // Adicionar o novo div ao container principal (main)
+  main.appendChild(newDiv);
 
-
-  
-})
+  // Limpar o campo de input após a inserção
+  input.value = "";
+});
